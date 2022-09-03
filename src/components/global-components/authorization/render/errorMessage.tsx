48ab4c4ca09ccf9abacdf2c533de4bb089a3839f
Sokolovskaya Lissa <sokolovskaya_lisa@mail.ru>
2022-09-03 17:32:53 +0400

    feat: add showPassword button

--- src/components/global-components/authorization/render/errorMessage.tsx
+++ src/components/global-components/authorization/render/errorMessage.tsx
@@ -1,19 +1,7 @@
-import { Alert, AlertDescription, AlertIcon, Box } from '@chakra-ui/react';
-
-export function ErrorMessage() {
-  return (
-    <Box my={4}>
-      <Alert status="error" borderRadius={4}>
-        <AlertIcon />
-        <AlertDescription>{}</AlertDescription>
-      </Alert>
-    </Box>
-  );
-}
-
 type props = {
   message: string;
 }
+
 export function ErrorDiv({ message }: props) {
   return (
     <div className="errorForm hidden" id="divError">
@@ -25,6 +13,7 @@ export function ErrorDiv({ message }: props) {
 type propsTwo = {
   message: string;
 }
+
 export function ErrorDivEmail({ message }: propsTwo) {
   return (
     <div className="errorForm hidden" id="divErrorEmail">
--- src/components/global-components/authorization/render/registration.tsx
+++ src/components/global-components/authorization/render/registration.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import { useForm } from 'react-hook-form';
 import './form.css';
-import { useContext } from 'react';
+import { BiHide, BiShow } from 'react-icons/bi';
+import { useContext, useState } from 'react';
 import { closeDivError, popupClose, viewButtonLogin } from '../utils/utils';
 import { AuthorizedCtx } from '../../../app/App';
 import { AuthorizeUser, createNewUser, upsertStatistics } from '../fetch/fetch';
@@ -10,6 +11,7 @@ import { ErrorDiv, ErrorDivEmail } from './errorMessage';
 
 export default function Registration() {
   const [, setAuthrize] = useContext(AuthorizedCtx);
+  const [showPassword, setShowPassword] = useState(true);
   type FormValues = {
     name: string;
     email: string;
@@ -25,6 +27,11 @@ export default function Registration() {
   } = useForm<FormValues>({
     mode: 'onBlur',
   });
+
+  function togglePasswordVisibility() {
+    return setShowPassword(!showPassword);
+  }
+
   const onSubmit = async (data: FormValues) => {
     const submitButtonReg = document.getElementById('idSubmitRegistration') as HTMLInputElement;
     submitButtonReg.disabled = true;
@@ -49,6 +56,7 @@ export default function Registration() {
     }
     submitButtonReg.disabled = false;
   };
+
   return (
     <form
       className="form_body registration hidden"
@@ -56,10 +64,10 @@ export default function Registration() {
     >
       <input
         {...register('name', {
-          required: 'Поле обязательно к заполнению',
+          required: 'The field must be filled in',
           minLength: {
             value: 3,
-            message: 'от 3 символов',
+            message: 'From 3 characters',
           },
         })
         }
@@ -73,10 +81,10 @@ export default function Registration() {
 
       <input
         {...register('email', {
-          required: 'Поле обязательно к заполнению',
+          required: 'The field must be filled in',
           pattern: {
             value: /^\S+@\S+$/,
-            message: "должен быть символ '@' ",
+            message: "There must be a symbol '@' ",
           },
         })
         }
@@ -84,31 +92,36 @@ export default function Registration() {
         className="popup_input registration"
         placeholder="Email Address"
       />
-      <div style={{ height: 25 }}>
+      <div className="error_message">
         {errors?.email && <p className="errorForm">{errors?.email?.message || 'Error!'}</p>}
       </div>
-      <input
-        {...register('password', {
-          required: 'Поле обязательно к заполнению',
-          minLength: {
-            value: 8,
-            message: 'от 8 до 12 символов',
-          },
-          maxLength: {
-            value: 12,
-            message: 'от 8 до 12 символов',
-          },
-        })
-        }
-        type="password"
-        className="password_input registration"
-        autoComplete="on"
-        placeholder="Password"
-      />
-      <div style={{ height: 25 }}>
+      <div className="password_form">
+        <input
+          {...register('password', {
+            required: 'The field must be filled in',
+            minLength: {
+              value: 8,
+              message: 'From 8 to 12 characters',
+            },
+            maxLength: {
+              value: 12,
+              message: 'From 8 to 12 characters',
+            },
+          })
+          }
+          type="password"
+          className="password_input registration"
+          autoComplete="on"
+          placeholder="Password"
+        />
+        <button type="button" className="password_btn" onClick={togglePasswordVisibility}>
+          {showPassword ? <BiHide /> : <BiShow />}
+        </button>
+      </div>
+      <div className="error_message">
         {errors?.password && <p className="errorForm">{errors?.password?.message || 'Error!'}</p>}
       </div>
-      <input type="submit" id="idSubmitRegistration" className="btn popup_btn registration" value="registration<" />
+      <input type="submit" id="idSubmitRegistration" className="btn popup_btn registration" value="registration" />
       <ErrorDiv message={errorLogin} />
       <ErrorDivEmail message={errorEmail} />
     </form>
