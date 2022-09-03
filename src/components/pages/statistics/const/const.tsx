64d7272315ea0bdefbc2fd8011c99f3854d7dc54
Sokolovskaya Lissa <sokolovskaya_lisa@mail.ru>
2022-09-03 17:33:05 +0400

    feat: change language in const

--- src/components/pages/statistics/const/const.tsx
+++ src/components/pages/statistics/const/const.tsx
@@ -2,9 +2,9 @@ export const BASELINK = 'https://react-learnwords-example.herokuapp.com';
 export const USERS = `${BASELINK}/users`;
 export const SIGNIN = `${BASELINK}/signin`;
 
-export const learnWords = 'Новых слов: ';
-export const procentRight = 'Правильных ответов: ';
-export const strick = 'Самая длинная серия правильных ответов: ';
+export const learnWords = 'New words: ';
+export const procentRight = 'Correct answers: ';
+export const strick = 'The longest series of correct answers: ';
 export const learnSvg = (<svg className="MuiSvgIconRoot" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M16.59 7.58L10 14.17l-3.59-3.58L5 12l5 5 8-8zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z" /></svg>);
 export const procentSvg = (<svg className="MuiSvgIconRoot" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" /></svg>);
 export const strictSvg = (
@@ -12,16 +12,16 @@ export const strictSvg = (
     <path d="M18 7l-1.41-1.41-6.34 6.34 1.41 1.41L18 7zm4.24-1.41L11.66 16.17 7.48 12l-1.41 1.41L11.66 19l12-12-1.42-1.41zM.41 13.41L6 19l1.41-1.41L1.83 12 .41 13.41z" />
   </svg>
 );
-export const titleAll = 'Статистика за сегодня';
-export const newWordDay = 'Новых слов';
-export const learnWordDay = 'Слов изучено';
-export const procentAnswerOfDay = 'Правильных ответов';
+export const titleAll = 'Statistics for today';
+export const newWordDay = 'New words';
+export const learnWordDay = 'Words learned';
+export const procentAnswerOfDay = 'Correct answers';
 export const amountDateLength = 10;
-export const grafTitleOne = 'Количество новых слов';
-export const grafTitleTwo = 'Количество изученных слов';
-export const axisX = 'период обучения';
-export const axisY = 'кол-во слов';
+export const grafTitleOne = 'Number of new words';
+export const grafTitleTwo = 'Number of words learned';
+export const axisX = 'training period';
+export const axisY = 'number of words';
 export const axisYOne = 'newWordsOfDay';
 export const axisYTwo = 'learnedWords';
-export const nameGameSprint = 'Спринт';
-export const nameGameAudioCall = 'Аудиовызов';
+export const nameGameSprint = 'Sprint';
+export const nameGameAudioCall = 'Audio call';
