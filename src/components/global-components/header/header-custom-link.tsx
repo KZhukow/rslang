f6223626b055b04819347fb109b003a412ce61be
Sokolovskaya Lissa <sokolovskaya_lisa@mail.ru>
2022-09-03 17:20:24 +0400

    feat: fix active link

--- src/components/global-components/header/header-custom-link.tsx
+++ src/components/global-components/header/header-custom-link.tsx
@@ -4,7 +4,9 @@ import { CustomLinkProp } from '../../pages/tutorial/types';
 
 export default function CustomHeaderLink({ children, to, className }: CustomLinkProp) {
   const match = useMatch(to);
-  const classNameActive = match ? ' nav-link-active' : '';
+  const darkLink = match ? ' nav_link_active dark_nav' : ' dark_nav';
+  const lightLink = match ? ' nav_link_active' : ' ';
+  const classNameActive = document.querySelector('.dark_body') ? darkLink : lightLink;
 
   return (
     <Link to={to} className={className + classNameActive}>{children}</Link>
