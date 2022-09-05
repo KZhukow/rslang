a56c42f476efc056d9b03f99e740df7b0258a754
Sokolovskaya Lissa <sokolovskaya_lisa@mail.ru>
2022-09-05 20:12:04 +0400

    feat: add adaptive

--- src/components/global-components/main-layout/main-layout.tsx
+++ src/components/global-components/main-layout/main-layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
-import Footer from '../footer/footer';
 import Header from '../header/header';
+import Footer from '../footer/footer';
 
 export default function MainLayout() {
   return (
